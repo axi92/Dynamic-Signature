e7803522e93c7121eaa1c253b77593c9903ac445
axi92 <phips_92@yahoo.de>
2012-11-28 21:46:31 +0100

    Update

--- 1.php
+++ 1.php
@@ -31,10 +31,13 @@ $phone = $row['PhoneNr'];
 $playtime = $row['ConnectedTime'];
 $frak_member = $row['Member'];
 $frak_leader = $row['Leader'];
-$geld = $money_bank + $money_hand;
-
+$geld = number_format($money_bank + $money_hand ,0,'','.');
 $kd = $kills / $deaths;
 $kd = round($kd,2);
+$job = $row['Job'];
+$rank = $row['Rank'];
+
+
 
 // FRAKTIONS DEFINITION ________________________________________
 $frak = "Zivilist";
@@ -63,83 +66,160 @@ $frak22 = "S.F.Rifa";
 $frak23 = "Red Soldiers";
 $frak24 = "L.S.Vagos";
 
+// JOB DEFINITION ________________________________________
+$job_name0 = "Arbeitslos";
+$job_name1 = "Detektiv";
+$job_name2 = "Anwalt";
+$job_name3 = "Hure";
+$job_name4 = "Drogendealer";
+$job_name5 = "Autodieb";
+$job_name6 = "Hacker";
+$job_name7 = "KFZ-Mechaniker";
+$job_name8 = "Bodyguard";
+$job_name9 = "Waffenhändler";
+$job_name10 = "Bauer";
+$job_name11 = "Hotdog-Verkäufer";
+$job_name12 = "Boxer";
+$job_name13 = "Arbeitslos";
+$job_name14 = "Tuner";
+$job_name15 = "Arbeitslos";
+$job_name16 = "Trucker";
+$job_name17 = "Arbeitslos";
+
+switch ($job)
+{
+    case "0":
+        $job = $job_name0;
+        break;
+    case "1":
+        $job = $job_name1;
+        break;
+    case "2":
+        $job = $job_name2;
+        break;
+    case "3":
+        $job = $job_name3;
+        break;
+    case "4":
+        $job = $job_name4;
+        break;
+    case "5":
+        $job = $job_name5;
+        break;
+    case "6":
+        $job = $job_name6;
+        break;
+    case "7":
+        $job = $job_name7;
+        break;
+    case "8":
+        $job = $job_name8;
+        break;
+    case "9":
+        $job = $job_name9;
+        break;
+    case "10":
+        $job = $job_name10;
+        break;
+    case "11":
+        $job = $job_name11;
+        break;   
+    case "12":
+        $job = $job_name12;
+        break;
+    case "13":
+        $job = $job_name13;
+        break;
+    case "14":
+        $job = $job_name14;
+        break;
+    case "15":
+        $job = $job_name15;
+        break;
+    case "16":
+        $job = $job_name16;
+        break;
+    case "17":
+        $job = $job_name17;
+        break; 
+}
 
 switch ($frak_member) 
 {
     case "-1":
-        $member = $frak;
+        $member = $frak . " Rang: " . $rank;
         break;
     case "2":
-        $member = $frak2;
+        $member = $frak2 . " Rang: " . $rank;
         break;
     case "3":
-        echo $frak3;
+        $member = $frak3 . " Rang: " . $rank;
         break;
     case "4":
-        $member = $frak4;
+        $member = $frak4 . " Rang: " . $rank;
         break;
     case "5":
-        $member = $frak5;
+        $member = $frak5 . " Rang: " . $rank;
         break;
     case "6":
-        $member = $frak6;
+        $member = $frak6 . " Rang: " . $rank;
         break;
     case "7":
-        $member = $frak7;
+        $member = $frak7 . " Rang: " . $rank;
         break;
     case "8":
-        $member = $frak8;
+        $member = $frak8 . " Rang: " . $rank;
         break;
     case "9":
-        $member = $frak9;
+        $member = $frak9 . " Rang: " . $rank;
         break;
     case "10":
-        $member = $frak10;
+        $member = $frak10 . " Rang: " . $rank;
         break;
     case "11":
-        $member = $frak11;
+        $member = $frak11 . " Rang: " . $rank;
         break;
     case "12":
-        $member = $frak12;
+        $member = $frak12 . " Rang: " . $rank;
         break;
     case "13":
-        $member = $frak13;
+        $member = $frak13 . " Rang: " . $rank;
         break;
     case "14":
-        $member = $frak14;
+        $member = $frak14 . " Rang: " . $rank;
         break;
     case "15":
-        $member = $frak15;
+        $member = $frak15 . " Rang: " . $rank;
         break;
     case "16":
-        $member = $frak16;
+        $member = $frak16 . " Rang: " . $rank;
         break;
     case "17":
-        $member = $frak17;
+        $member = $frak17 . " Rang: " . $rank;
         break;
     case "18":
-        $member = $frak18;
+        $member = $frak18 . " Rang: " . $rank;
         break;
     case "19":
-        $member = $frak19;
+        $member = $frak19 . " Rang: " . $rank;
         break;
     case "20":
-        $member = $frak20;
+        $member = $frak20 . " Rang: " . $rank;
         break;
     case "21":
-        $member = $frak21;
+        $member = $frak21 . " Rang: " . $rank;
         break;
     case "21":
-        $member = $frak21;
+        $member = $frak21 . " Rang: " . $rank;
         break;
     case "22":
-        $member = $frak22;
+        $member = $frak22 . " Rang: " . $rank;
         break;
     case "23":
-        $member = $frak23;
+        $member = $frak23 . " Rang: " . $rank;
         break;
     case "24":
-        $member = $frak24;
+        $member = $frak24 . " Rang: " . $rank;
         break;
 }
 
@@ -241,6 +321,7 @@ ImageString ($img, 4, 10, 35, 'Geld: ' . $geld . '$', $text);
 ImageString ($img, 4, 10, 50, 'KD: ' . $kd, $text);
 ImageString ($img, 4, 10, 65, 'Number: ' . $phone, $text);
 
-ImageString ($img, 4, 150, 5, 'Fraktion: ' . $member, $text);
+ImageString ($img, 4, 180, 5, '' . $member, $text);
+ImageString ($img, 4, 180, 20, 'Job: ' . $job, $text);
 ImageJPEG ($img);
 ?>
